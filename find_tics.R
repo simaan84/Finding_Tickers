d3cdf507338eacc01f88007bf9e1c6a75ca27194
Majeed Simaan <simaan84@gmail.com>
2018-05-15 14:43:59 -0400

    Add files via upload

---
+++ find_tics.R
@@ -0,0 +1,35 @@
+library(rvest)
+
+rm(list = ls())
+
+get_tics <- function(theurl) {
+  file <- read_html(theurl)
+  tables <- html_nodes(file, "table")
+
+  # try extract each into a data.frame
+  tables.list <- numeric()
+  for (i in 1:length(tables)) { 
+    tables.list[i] <- list(try(html_table(tables[i], fill = TRUE),silent = T))
+  }
+
+  # the table we are looking for is found here
+  ds <- tables.list[[3]][[1]]
+  names(ds) <- ds[1,]
+  ds <- ds[-1,]
+
+  return(ds)
+}
+
+# scrap the data from online
+theurl1 <- "http://www.wsj.com/mdc/public/page/2_3021-activnyse-actives.html"
+ds1 <- get_tics(theurl1)
+tics <- sapply(strsplit(ds1[,2]," "),function(x) x[length(x)]) 
+tics <- gsub("\\(","",tics)
+tics <- gsub("\\)","",tics)
+cat("The most traded tickers are\n", tics[1:10],"\n")
+
+# the same function can be applied to the top gainers table
+theurl2 <- "http://www.wsj.com/mdc/public/page/2_3021-gainnyse-gainer.html?mod=mdc_uss_pglnk"
+ds2 <- get_tics(theurl2)
+head(ds2)
+
